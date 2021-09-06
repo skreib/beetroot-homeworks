8ef229642007bae3f6ee4223093ce29fd9697ccf
Roman Vitruk <skreib@gmail.com>
2021-09-06 20:49:41 +0300

    add some code

---
+++ 06-PHP-vaiables/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE>
+<html>
+    <head>
+        <title>PHP Variables</title>
+    </head>
+    <body>
+        <div id="app">
+            <form action="">
+            </form>
+        </div>
+    </body>
+</html>
